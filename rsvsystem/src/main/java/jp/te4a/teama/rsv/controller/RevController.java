c52c63ba015503af28b851559da256dbe40a9bff
loop0919 <ruupu2001@gmail.com>
2024-07-10 13:59:38 +0900

    [WIP] Spring Boot に適応させる

---
+++ rsvsystem/src/main/java/jp/te4a/teama/rsv/controller/RevController.java
@@ -0,0 +1,16 @@
+package jp.te4a.teama.rsv.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path="")
+public class RevController {
+    @GetMapping(path="home")
+    public String home(Model model) {
+        return "home";
+    }
+}
+
